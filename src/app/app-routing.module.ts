2e0faab3ffe5746b41a49732455db048b3f0b584
Sam Dickinson <sam.t.dickinson@gmail.com>
2023-03-02 08:37:09 +0000

    Added statistics component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {ViewsModule} from "./views/views.module";
 import {NotFoundComponent} from "./not-found/not-found.component";
 
 const routes: Routes = [
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path: 'collections', loadChildren: () => import('./collections/collections.module').then(
       (m) => m.CollectionsModule
+    )
+  },
+  {
+    path: 'views', loadChildren: () => import('./views/views.module').then(
+      (m) => m.ViewsModule
     )
   },
   {path: '', component: HomeComponent},
---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css']
+})
+export class ViewsHomeComponent {
+ stats = [
+   {value: 22, label: '# of users'},
+   {value: 900, label:' Revenue'},
+   {value: 50, label: 'Satisfaction Score'}
+ ]
+}
