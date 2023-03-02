7023fc65e52934628bac7fe571e891ccc379c0ce
Sam Dickinson <sam.t.dickinson@gmail.com>
2023-03-02 11:43:14 +0000

    Added Item list component and some styling

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-views-home',
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./views-home.component.css']
 })
 export class ViewsHomeComponent {
- stats = [
-   {value: 22, label: '# of users'},
-   {value: 900, label:' Revenue'},
-   {value: 50, label: 'Satisfaction Score'}
- ]
+  stats = [
+    {value: 22, label: '# of users'},
+    {value: 900, label: ' Revenue'},
+    {value: 50, label: 'Satisfaction Score'}
+  ];
+
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch'
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a fantastic dresser'
+    }
+  ];
 }
